fe41a544b3079208550798e5f8a0e67970814734
Mika Lim <lslim@usc.edu>
2025-01-29 15:18:39 -0800

    commented out tester

--- test_split.cpp
+++ test_split.cpp
@@ -1,54 +1,54 @@
-/*
-CSCI 104: Homework 1 Problem 1
+// /*
+// CSCI 104: Homework 1 Problem 1
 
-Use this file to test your split.cpp file.
+// Use this file to test your split.cpp file.
 
-Create linked lists and split them with your split() function.
+// Create linked lists and split them with your split() function.
 
-You can compile this file like this:
-g++ split.cpp test_split.cpp -o test_split
-*/
+// You can compile this file like this:
+// g++ split.cpp test_split.cpp -o test_split
+// */
 
-#include "split.h"
-#include <iostream>
+// #include "split.h"
+// #include <iostream>
 
-int main(int argc, char* argv[])
-{
+// int main(int argc, char* argv[])
+// {
 
 
-//List is [1,2,4,3]
-Node* four =new  Node(3,nullptr);
-Node* three = new Node(4,four);
-Node* two = new Node(2, three);
-Node* one = new Node(1,two);
+// //List is [1,2,4,3]
+// Node* four =new  Node(3,nullptr);
+// Node* three = new Node(4,four);
+// Node* two = new Node(2, three);
+// Node* one = new Node(1,two);
 
-//verify i set up this list correctly LOL
+// //verify i set up this list correctly LOL
 
- Node* current = one;
+//  Node* current = one;
+// // while(current != nullptr){
+// //     std::cout << current->value << std::endl;
+// //     current = current->next;
+// // }
+// Node* odds = nullptr;
+// Node* evens = nullptr;
+// Node* nullInput = nullptr;
+
+// split(nullInput,odds, evens);
+
+
+// std::cout << "odds:" << std::endl;
+// current = odds;
 // while(current != nullptr){
 //     std::cout << current->value << std::endl;
 //     current = current->next;
 // }
-Node* odds = nullptr;
-Node* evens = nullptr;
-Node* nullInput = nullptr;
 
-split(nullInput,odds, evens);
-
-
-std::cout << "odds:" << std::endl;
-current = odds;
-while(current != nullptr){
-    std::cout << current->value << std::endl;
-    current = current->next;
-}
-
-std::cout << "evens:" << std::endl;
-current = evens;
-while(current != nullptr){
-    std::cout << current->value << std::endl;
-    current = current->next;
-}
+// std::cout << "evens:" << std::endl;
+// current = evens;
+// while(current != nullptr){
+//     std::cout << current->value << std::endl;
+//     current = current->next;
+// }
  
-}
+// }
 
