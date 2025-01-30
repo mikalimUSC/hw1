9e935eeb10270416717d34a99238b000477df264
Mika Lim <lslim@usc.edu>
2025-01-29 22:01:43 -0800

    more debug

--- split.cpp
+++ split.cpp
@@ -11,6 +11,7 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <iostream>
 using namespace std;
 
 /* Add a prototype for a helper function here if you need */
@@ -23,6 +24,7 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   odds = nullptr;
   evens = nullptr;
+
   splithelp(in,odds,evens);
   in = nullptr;
 }
@@ -31,46 +33,65 @@ void split(Node*& in, Node*& odds, Node*& evens)
 
 void splithelp(Node*& node, Node*& odds, Node*& evens)
 {
+  cout<< "splithelp: node is " << node->value<<endl;
+ 
   if(node == nullptr){
     return;
   }
- 
+  Node*next = nullptr;
+  if(node->next !=nullptr){
+    cout<< " Node-> next not equal to nullptr , val is " << node->next->value<<endl;
+     next = node->next;
+  }
+
   if(node->value %2 ==0){
+     cout<< "Adding" << node ->value << " to evens"<<endl;
     evens = add(evens,node);
     
   }else{
+    cout<< "Adding" << node ->value << " to odds"<<endl;
     odds = add(odds,node);
+    
+
+
   }
 
-  Node* next = node->next;
+
+  if(next !=nullptr){
   splithelp(next,odds,evens);
-   delete node;
+  }
+
 
 
 }
 
 Node* add(Node*& list, Node*& node){
+ 
   if(node == nullptr){
     return list;
   }
 
   if(list == nullptr ){
+      cout << "add: node value is " << node->value<< " list  is null"<< endl;
     //  Node* newNode = new Node(node->value,nullptr);
     list = node;
     node->next = nullptr;
-
     return list;
   }
+
+  
   if(list->next == nullptr){
-    
+    cout << "add: node value is " << node->value<< " list is " << list->value << " also listnext null"<< endl;
     // Node* newNode = new Node(node->value,nullptr);
-    
+   
     list->next = node;
     node->next = nullptr;
     return list;
   }
 
+  cout << "add: node value is " << node->value<< " list is " << list->value<< endl;
   add(list->next,node);
+ 
   return list;
 
 }
